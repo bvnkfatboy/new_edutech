9780ff15e02bf2c3fd55392295a42955b39e56b2
Theerapong Bank <theerapong.bvnk@gmail.com>
2024-01-08 15:20:02 +0700

    up

---
+++ dist/js/tech_script.js
@@ -0,0 +1,27 @@
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    navText: [
+      "<i class='fas fa-chevron-circle-left'></i>",
+      "<i class='fas fa-chevron-circle-right'></i>"
+    ],
+    autoplay: true,
+    autoplayHoverPause: true,
+    responsiveClass:true,
+    
+    responsive: {
+      0: {
+        items: 1
+      },
+      500: {
+        items: 2
+      },
+      600: {
+        items: 3
+      },
+      1000: {
+        items: 4
+      }
+    }
+  })

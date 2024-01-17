3ab6e06dff35e5d2284f2140f83a85089c93b224
surakid <156279778+surakid@users.noreply.github.com>
2024-01-17 09:34:58 +0700

    up

--- dist/js/tech_script.js
+++ dist/js/tech_script.js
@@ -25,3 +25,31 @@ $('.tech-body .owl-carousel').owlCarousel({
       }
     }
   })
+
+  $('.tech-slide-img .owl-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    navText: [
+      "<i class='fas fa-chevron-circle-left'></i>",
+      "<i class='fas fa-chevron-circle-right'></i>"
+    ],
+    autoplay: true,
+    autoplayHoverPause: true,
+    responsiveClass:true,
+    
+    responsive: {
+      0: {
+        items: 1
+      },
+      500: {
+        items: 2
+      },
+      600: {
+        items: 3
+      },
+      1000: {
+        items: 3
+      }
+    }
+  })

5e53409dff73bb7fe5fd0594a55450ed0127c215
Theerapong Bank <theerapong.bvnk@gmail.com>
2024-01-17 09:56:59 +0700

    up

--- dist/js/tech_script.js
+++ dist/js/tech_script.js
@@ -1,4 +1,4 @@
-$('.tech-body .owl-carousel').owlCarousel({
+$('.tech-slide-image .owl-carousel , .tech-slide-blog .owl-carousel , .tech-slide-video .owl-carousel').owlCarousel({
     loop: true,
     margin: 10,
     nav: true,
@@ -29,15 +29,15 @@ $('.tech-body .owl-carousel').owlCarousel({
   $('.tech-slide-img .owl-carousel').owlCarousel({
     loop: true,
     margin: 10,
-    nav: true,
+    nav: false,
     navText: [
       "<i class='fas fa-chevron-circle-left'></i>",
       "<i class='fas fa-chevron-circle-right'></i>"
     ],
     autoplay: true,
+    autoplayTimeout: 3000,
     autoplayHoverPause: true,
     responsiveClass:true,
-    
     responsive: {
       0: {
         items: 1
@@ -46,7 +46,7 @@ $('.tech-body .owl-carousel').owlCarousel({
         items: 2
       },
       600: {
-        items: 3
+        items: 2
       },
       1000: {
         items: 3

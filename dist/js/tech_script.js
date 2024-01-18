27b172737d616fa4c4ea7b35631bd12bae66a8cc
Theerapong Bank <theerapong.bvnk@gmail.com>
2024-01-18 10:05:48 +0700

    up

--- dist/js/tech_script.js
+++ dist/js/tech_script.js
@@ -53,31 +53,3 @@ $('.tech-slide-image .owl-carousel , .tech-slide-blog .owl-carousel , .tech-slid
       }
     }
   })
-
-  $('.tech-slide-img .owl-carousel').owlCarousel({
-    loop: true,
-    margin: 10,
-    nav: false,
-    navText: [
-      "<i class='fas fa-chevron-circle-left'></i>",
-      "<i class='fas fa-chevron-circle-right'></i>"
-    ],
-    autoplay: true,
-    autoplayTimeout: 3000,
-    autoplayHoverPause: true,
-    responsiveClass:true,
-    responsive: {
-      0: {
-        items: 1
-      },
-      500: {
-        items: 2
-      },
-      600: {
-        items: 2
-      },
-      1000: {
-        items: 3
-      }
-    }
-  })

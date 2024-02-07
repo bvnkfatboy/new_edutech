9edce4085929b9a9f44e3b348148f92242c1c1e4
Theerapong Bank <theerapong.bvnk@gmail.com>
2024-02-07 13:46:20 +0700

    up

--- dist/js/tech_script.js
+++ dist/js/tech_script.js
@@ -26,30 +26,30 @@ $('.tech-slide-video .owl-carousel').owlCarousel({
     }
   })
 
-  $('.tech-slide-img .owl-carousel').owlCarousel({
-    loop: true,
-    margin: 10,
-    nav: false,
-    navText: [
-      "<i class='fas fa-chevron-circle-left'></i>",
-      "<i class='fas fa-chevron-circle-right'></i>"
-    ],
-    autoplay: true,
-    autoplayTimeout: 3000,
-    autoplayHoverPause: true,
-    responsiveClass:true,
-    responsive: {
-      0: {
-        items: 1
-      },
-      500: {
-        items: 2
-      },
-      600: {
-        items: 2
-      },
-      1000: {
-        items: 3
-      }
-    }
-  })
+  // $('.tech-slide-img .owl-carousel').owlCarousel({
+  //   loop: true,
+  //   margin: 10,
+  //   nav: false,
+  //   navText: [
+  //     "<i class='fas fa-chevron-circle-left'></i>",
+  //     "<i class='fas fa-chevron-circle-right'></i>"
+  //   ],
+  //   autoplay: true,
+  //   autoplayTimeout: 3000,
+  //   autoplayHoverPause: true,
+  //   responsiveClass:true,
+  //   responsive: {
+  //     0: {
+  //       items: 1
+  //     },
+  //     500: {
+  //       items: 2
+  //     },
+  //     600: {
+  //       items: 2
+  //     },
+  //     1000: {
+  //       items: 3
+  //     }
+  //   }
+  // })
